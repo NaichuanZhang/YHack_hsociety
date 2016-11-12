f66a7ddb84ca76a4c1eeab3944b8f200f3591308
alex <alex@alexs-MacBook.local>
2016-11-12 13:09:30 -0500

    home_page

---
+++ app.py
@@ -0,0 +1,36 @@
+import flask
+from flask import Flask, Response, request, render_template, redirect, url_for, send_from_directory, session
+from flaskext.mysql import MySQL
+import flask.ext.login as flask_login
+from flask_oauth import OAuth
+from flask_googlemaps import GoogleMaps
+from flask_googlemaps import Map
+import json
+import os
+
+#database setup
+'''
+mysql = MySQL()
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'your database password'
+app.config['MYSQL_DATABASE_DB'] = 'roundtable'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
+'''
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def welcome():
+    return render_template('index.html')
+
+app.config.update(
+    DEBUG = True,
+)
+
+
+if __name__ == "__main__":
+	#this is invoked when in the shell  you run
+	#$ python app.py
+	app.run(port=5000, debug=True)
